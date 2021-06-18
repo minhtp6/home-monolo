78537995ef95db115c41e6e8c42eed096f7779e9
minhpt6 <minhtp@lattehub.com>
2021-06-18 15:50:37 +0700

    new

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <Header/>
-    
+   
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+
+
 export default {
   name: 'App',
   components: {
-    Header,
+ 
   }
 }
 </script>
